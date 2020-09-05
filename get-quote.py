675f16d2155262704aa49053edeab78fec8a652b
baiker99 <stefan.baiker@gmx.net>
2020-09-05 23:50:17 +0200

    Update get-quote.py
    Responsive is better than fast

--- get-quote.py
+++ get-quote.py
@@ -1,11 +1,11 @@
-def weissnicht():
-  print("Keep it logically awesome.")
-  print("Keep it logically awesome.")
-  #f = open("quotes.txt")
-  #quotes = f.readlines()
-  #f.close()
+def main():
+  #print("Keep it logically awesome.")
 
-  #print(quotes)
+  f = open("quotes.txt")
+  quotes = f.readlines()
+  f.close()
+
+  print(quotes[0])
 
 if __name__== "__main__":
-  weissnicht()
+  main()
