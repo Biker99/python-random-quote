5674df05af9a45b48ea0fd0a64e8ad4b8e0a245a
baiker99 <stefan.baiker@gmx.net>
2020-09-05 23:58:32 +0200

    Update get-quote.py

--- get-quote.py
+++ get-quote.py
@@ -1,11 +1,11 @@
 def main():
-  #print("Keep it logically awesome.")xcvx
+  #print("Keep it logically awesome.")
 
   f = open("quotes.txt")
   quotes = f.readlines()
   f.close()
 
-  print(quotes[0])
+  print(quotes[13])
 
 if __name__== "__main__":
   main()
